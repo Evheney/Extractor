a3fb8a3ed473adbb04169efb5db0f4ff4d27be64
Eugene <ealpha11@gmail.com>
2021-08-16 18:32:38 +0900

    Declared parser settigns and made console arguments

--- main.cpp
+++ main.cpp
@@ -11,6 +11,7 @@
 #include <cstdlib>
 #include <QProcess>
 
+#include <QCommandLineParser>
 #include <qtconcurrentmap.h>
 
 using namespace QtConcurrent;
@@ -70,8 +71,31 @@ int main(int argc, char *argv[])
 {
     QCoreApplication a(argc, argv);
     //QApplication app(argc, argv);
-    qDebug() << "finding files...";
-    const QStringList files = findFiles("D:/Eugene/Damkook/TestingFilesApk", QStringList() << "*.apk" );
+
+    // DECLARING parser and setting default options and positional arguments
+    QCommandLineParser parser;
+    parser.setApplicationDescription(QObject::tr("Manifest files extractor."));
+    parser.addPositionalArgument("source apk directory ", QObject::tr("Input source directory."));
+
+    // PARSING options
+    parser.process(a);
+
+    // CHECKING and USING options
+    const QStringList args = parser.positionalArguments();
+    for (auto& a: args)
+        qDebug() << "Arg:" << a;
+    QString sourcedir;// = "D:/Eugene/Damkook/TestingFilesApk";
+    if(args.size() > 0 ){
+        sourcedir = args.at(0);
+    } else {
+    qDebug() << "Specify source directory as first agrument, e.g.";
+    qDebug() << ">extractor D:/APK_source_folder/";
+    return 0;
+    }
+
+    qDebug() << "Finding APK Source files in " << sourcedir;
+
+    const QStringList files = findFiles(sourcedir, QStringList() << "*.apk" );
     qDebug() << files.count() << "files";
 
     for(const QString &apkName : files) {
@@ -79,7 +103,6 @@ int main(int argc, char *argv[])
         QString line = fi.path()+"/"+fi.baseName();
         qDebug() << line;
     }
-
 /*
     QTime time;
     time.start();
@@ -105,7 +128,8 @@ int main(int argc, char *argv[])
         map(files, Extract);
 
         mapTime = time.elapsed();
-        qDebug() << "Map time" << mapTime << "ms";
+        qDebug() << "Extractor finished extracting apk files in:" << mapTime<< "ms";
+        //qDebug() << "Map time" << mapTime << "ms";
     }
 
     return a.exec();
